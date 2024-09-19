7d67d030e1298739906b709af4b97ad22927fe24
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-09-19 08:51:42 -0500

    T1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double SalarioBruto, SalarioNeto = 0, TotalG, Ventas, Años, comision = 0, descuento = 0;
+string nombre;
+
+Console.Write("\nIngrese su nombre completo: ");
+nombre = Console.ReadLine().ToUpper().Trim();
+Console.Write("\nIngrese cuanto vendiste: ");
+Ventas = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nIngrese su salario: ");
+SalarioBruto = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nIngrese cuantos años llevas en la empresa: ");
+Años = Convert.ToDouble(Console.ReadLine());
+
+if (Ventas <= 600 && Ventas >= 0 )
+{
+    comision = Ventas + Ventas * 0.05;
+}
+else if (Ventas > 600 && Ventas <= 3000 )
+{
+    comision = Ventas + Ventas * 0.07;
+}
+else if (Ventas > 3000)
+{
+    comision = Ventas + Ventas * 0.06 + 300;
+}
+
+TotalG = SalarioBruto + comision;
+
+if (Años > 0 && Años <= 3)
+{
+    descuento = TotalG * 0.65;
+    SalarioNeto = TotalG - descuento;
+}
+if (Años > 4)
+{
+    descuento = TotalG * 0.45;
+    SalarioNeto = TotalG - descuento;
+}
+
+Console.WriteLine("\n\nNombre: " + nombre);
+Console.WriteLine("\nEl total que gano: " + TotalG);
+Console.WriteLine("\nLa cantidad que se le descontara: " + descuento);
+Console.WriteLine("\nEl total seria: " + SalarioNeto);
+
+
+
---
+++ T1 pregunta 2/Program.cs
@@ -0,0 +1,36 @@
+﻿int placa = 0, contador = 1;
+string color;
+
+Console.WriteLine("Ingrese Los colores de la calcamia que llevaban los autos que pasaron");
+
+
+while (contador >= 1)
+{ 
+    
+    color = Console.ReadLine().ToUpper().Trim();
+
+    if (color == "VERDE")
+    {
+        contador += 1;
+        for (placa += 1; placa <= 3 && placa >= 0;) { placa += 1; }     
+    }
+    else if (color == "AZUL")
+    {
+        contador += 1;
+        for (placa += 1; placa <= 6 && placa > 3;) { placa += 1; }
+    }
+    else if (color == "ROJO")
+    {
+        contador += 1;
+        for (placa += 1; placa <= 9 && placa > 6;) { placa += 1; }
+    }
+    else if (color == "F")
+    {
+        contador -= 1;
+        
+        Console.WriteLine($"{placa}, "); 
+        
+        break;
+    }
+    
+}
